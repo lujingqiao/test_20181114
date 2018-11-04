ef61c3ae0b0b82de3609a1ab1807c8b6fc222ddb
lujingqiao <lujingqiao@zuzuche.com>
2018-11-04 18:50:28 +0800

    iiii

---
+++ test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+#@Time: 2018/11/4 18:45
+#@Author: lujingqiao
+#@FileName: test.py
+#@Software: PyCharm
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
